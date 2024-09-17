de17041bc13d32fef876c54bbff366ecc598f1ec
Akashsaw6299 <144892204+Akashsaw6299@users.noreply.github.com>
2024-09-17 20:28:42 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let menuBtn = document.querySelector("#menu-btn");
-let navbar = document.querySelector('.navbar');
-
-menuBtn.addEventListener("click", function () {
-    console.log("clicked");
-    menuBtn.classList.toggle('fa-times');
-    navbar.classList.toggle('open');
-});
